77f43d6790665d2b357e425857460c210103d092
Leandro-Silva-Santos <leandro.ls.silva.santos@gmail.com>
2022-01-24 10:48:10 -0300

    teste no netbeans do leandro

--- Projeto Tecsenior MVC/src/Tecsenior/Model/Cargo.java
+++ Projeto Tecsenior MVC/src/Tecsenior/Model/Cargo.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Tecsenior.Model;
 
-/**
- *
- * @author Diogo Santos
- */
 public class Cargo {
     
-    private Integer idCargo;
-    private String Nome;
-    private Double Salario;
-    private Integer Carga_horaria;
-    private String Descricao;
-    
+    private int idCargo;
+    private String nome;
+    private double salario;
+    private int carga_horaria;
+    private String descricao;
     
 }
